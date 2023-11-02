9aeec7710b848910e32c7ee251cfa95d3c689cf6
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-11-02 20:18:51 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
+class Key { 
+}
 
+class MyHouse {
+  private doorIsOpen: boolean = false;
+
+  constructor(private key: Key) {   
+  }
+
+  openDoor(key: Key): void {
+    if (this.key === key) {
+      this.doorIsOpen = true;
+      console.log('Двері відчинені.');
+    } else {
+      console.log('Неправильний ключ. Двері не відкрилися.');
+    }
+  }
+
+  comeIn(person: Person): void {
+    if (this.doorIsOpen) {
+      console.log(`${person.getName()} входить у будинок.`);
+    } else {
+      console.log('Двері закриті. Спробуйте відкрити двері ключем.');
+    }
+  }
+}
+
+class Person {
+  constructor(private key: Key) {   
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+
+  getName(): string {
+    
+    return 'John Doe';
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
